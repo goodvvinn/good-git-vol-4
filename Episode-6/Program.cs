3ad2aab3dd16a5d7c7397bdb085a33dec71d496f
goodvvinn <goodvvinn@gmail.com>
2021-02-12 22:57:48 +0200

    StyleCop added to Ep6

--- Episode-6/Program.cs
+++ Episode-6/Program.cs
@@ -2,11 +2,64 @@
 
 namespace Episode_6
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private enum Alphabet
         {
-            Console.WriteLine("Hello World!");
+            A,
+            B,
+            C,
+            D,
+            E,
+            F,
+            G,
+            H,
+            I,
+            J,
+            K,
+            L,
+            M,
+            N,
+            O,
+            P,
+            Q,
+            R,
+            S,
+            T,
+            U,
+            V,
+            W,
+            X,
+            Y,
+            Z
+        }
+
+        private static void Main(string[] args)
+        {
+            string lenght = Console.ReadLine();
+            bool res = int.TryParse(lenght, out int a);
+            int[] myArray = new int[a];
+            Random random = new Random();
+            if (res)
+            {
+                myArray = new int[a];
+                Console.WriteLine("Lenght of the array is {0}", a);
+            }
+            else
+            {
+                Console.WriteLine("Wrong value, Please enter a valid number of elements in the array");
+            }
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = random.Next(1, 27);
+            }
+
+            // Console.WriteLine("The array contains : ");
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
         }
     }
 }
